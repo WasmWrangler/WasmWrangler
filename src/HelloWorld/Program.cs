a5136f5651e41187cfd7aa12f0ae360cba1f9994
smack0007 <zachary.snow@gmail.com>
2020-11-05 16:02:46 +0100

    Initial commit.

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WebAssembly;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void WasmMain()
+        {
+            Console.WriteLine("Hello World from WASM!");
+
+            var app = Runtime.GetGlobalObject("App") as JSObject;
+            app.Invoke("sayHello", "smack0007");
+        }
+    }
+}
