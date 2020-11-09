3cbb5a041f90a03249831a9c95d256c2a9c21b8b
smack0007 <zachary.snow@gmail.com>
2020-11-09 15:01:43 +0100

    TypeScript will now be compiled.

---
+++ samples/HelloWorld/scripts/app.ts
@@ -0,0 +1,15 @@
+﻿import { greet } from "./greeter";
+
+declare var BINDING: any;
+
+const App = {
+    init: function () {
+        document.getElementById("loading").style.display = 'none';
+        document.getElementById("app").style.display = 'block';
+        BINDING.call_static_method("[HelloWorld] HelloWorld.Program:WasmMain");
+    },
+
+    sayHello: function (name) {
+        console.info(greet(name));
+    }
+};
---
+++ samples/HelloWorld/scripts/greeter.ts
@@ -0,0 +1,3 @@
+﻿export function greet(name: string): string {
+    return `Hello ${name}`;
+}
