05349b8bb28b5efad67733793f29ad884d918e69
smack0007 <zachary.snow@gmail.com>
2020-11-10 17:25:55 +0100

    App is now a class with static methods.

--- samples/HelloWorld/scripts/app.ts
+++ samples/HelloWorld/scripts/app.ts
@@ -2,21 +2,21 @@
 
 declare var BINDING: any;
 
-const App = {
-    clickCount: 0,
+class App {
+    public static clickCount: number = 0;
 
-    init: function () {
+    public static init(): void {
         document.getElementById("loading").style.display = 'none';
         document.getElementById("app").style.display = 'block';
         BINDING.call_static_method("[HelloWorld] HelloWorld.App:WasmMain");
-    },
+    }
 
-    sayHello: function (name: string): void {
+    public static sayHello(name: string): void {
         console.info(greet(name));
-    },
+    }
 
-    onClick: function (button: HTMLButtonElement) {
+    public static onClick(button: HTMLButtonElement): void {
         BINDING.call_static_method("[HelloWorld] HelloWorld.App:IncrementClickCount");
         button.innerText = `Clicked ${App.clickCount} times`;
     }
-};
+}
