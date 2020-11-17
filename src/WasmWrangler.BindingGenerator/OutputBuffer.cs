6ad6acd1bc065581b96a506608eb5106157966bd
smack0007 <zachary.snow@gmail.com>
2020-11-17 21:07:32 +0100

    Refactored how the indenting works in the BindingsGenerator.

---
+++ src/WasmWrangler.BindingGenerator/OutputBuffer.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace WasmWrangler.BindingGenerator
+{
+    public class OutputBuffer
+    {
+        private readonly StringBuilder _sb;
+        private int _indentLevel;
+        private bool _isNewLine = true;
+
+        public OutputBuffer()
+        {
+            _sb = new StringBuilder(1024);
+        }
+
+        public override string ToString() => _sb.ToString();
+
+        public void IncreaseIndent() => _indentLevel++;
+
+        public void DecreaseIndent() => _indentLevel--;
+
+        public void Append(string value)
+        {
+            EnsureIndent();
+            _sb.Append(value);
+        }
+
+        public void AppendLine()
+        {
+            _sb.AppendLine();
+            _isNewLine = true;
+        }
+
+        public void AppendLine(string value)
+        {
+            EnsureIndent();
+            _sb.AppendLine(value);
+            _isNewLine = true;
+        }
+
+        private void EnsureIndent()
+        {
+            if (_isNewLine && _indentLevel > 0)
+                _sb.Append(new string('\t', _indentLevel));
+
+            _isNewLine = false;
+        }
+    }
+}
