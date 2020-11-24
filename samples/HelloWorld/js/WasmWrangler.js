4b0d9f72377d5b56d9a7fa4c236319e397d23b33
smack0007 <zachary.snow@gmail.com>
2020-11-24 21:33:39 +0100

    Copy WasmWrangler.js.map as well.

--- samples/HelloWorld/js/WasmWrangler.js
+++ samples/HelloWorld/js/WasmWrangler.js
@@ -1,6 +1,14 @@
-class Wasm {
-    static callStaticMethod(assemblyName, type, methodName, ...args) {
-        BINDING.call_static_method(`[${assemblyName}] ${type}:${methodName}`);
+"use strict";
+var Wasm = /** @class */ (function () {
+    function Wasm() {
     }
-}
+    Wasm.callStaticMethod = function (assemblyName, type, methodName) {
+        var args = [];
+        for (var _i = 3; _i < arguments.length; _i++) {
+            args[_i - 3] = arguments[_i];
+        }
+        BINDING.call_static_method("[" + assemblyName + "] " + type + ":" + methodName);
+    };
+    return Wasm;
+}());
 //# sourceMappingURL=WasmWrangler.js.map
